29bdd9a7fee39f0d03424502d6ae4fcf5d52ebb5
xiao.xinmiao <xiaoxm_001@13..com>
2017-10-19 16:10:12 +0800

    delete by id

--- bs/first.py
+++ bs/first.py
@@ -8,76 +8,82 @@ browser.find_element_by_id("txtUserPassword").send_keys("123456")
 browser.find_element_by_id("btnLogin").click() 
 
 
-id = 11112222
+id = "11112222"
+
+sleepTime = 3
+
+
 
 time.sleep(10)
 
 browser.find_element_by_xpath('//*[@id="app-menu-placeholder-targetEl"]/div/img[@id="tool-1021-toolEl"]').click()
-time.sleep(5)
+time.sleep(sleepTime)
 browser.find_element_by_xpath('//*[@id="app-menu-body"]/div/table/tbody//div/span[text()="系统管理"]').click()
 
-time.sleep(5)
+time.sleep(sleepTime)
 browser.find_element_by_xpath('//*[@id="app-menu-body"]/div/table/tbody//div/span[text()="用户管理"]').click()
 
-time.sleep(5)
+time.sleep(sleepTime)
 browser.find_element_by_xpath('//*[@id="app-menu-body"]/div/table/tbody//div/span[text()="用户资料维护"]').click()
 
-time.sleep(5)
+time.sleep(sleepTime)
 
-browser.find_element_by_xpath('//*[@id="app-tabs-body"]//div/a/span/span/span[text()=" 新增 "]').click()
+for index in range(0,2):
+    browser.find_element_by_xpath('//*[@id="app-tabs-body"]//div/a/span/span/span[text()=" 新增 "]').click()
+    #1.新增
+    time.sleep(sleepTime)
+
+    browser.switch_to.frame(browser.find_element_by_xpath("//iframe[contains(@src,'staff.aspx?opmode=m&menuid=110306')]"))
+
+    time.sleep(sleepTime)
+
+    browser.find_element_by_id("TabContainer1_tabDetail_tbStaffCode2").send_keys(int(id)+index)
+    browser.find_element_by_id("TabContainer1_tabDetail_tbStaffName2").send_keys("肖新苗")
+    browser.find_element_by_id("TabContainer1_tabDetail_tbPwd").send_keys("1234")
+
+    browser.switch_to.parent_frame()
+
+    time.sleep(sleepTime)
+
+    browser.find_element_by_xpath('//*[@id="app-tabs-body"]//div/a/span/span/span[text()=" 保存 "]').click()
+
+    time.sleep(sleepTime)
+    browser.find_element_by_xpath('//*[@id="messagebox-1001"]//div/a[2]').click()
+    time.sleep(sleepTime)
+    browser.switch_to.parent_frame()
 
 
-#1.新增
-time.sleep(5)
-
-browser.switch_to.frame(browser.find_element_by_xpath("//iframe[contains(@src,'staff.aspx?opmode=m&menuid=110306')]"))
-
-time.sleep(5)
-
-browser.find_element_by_id("TabContainer1_tabDetail_tbStaffCode2").send_keys(id)
-browser.find_element_by_id("TabContainer1_tabDetail_tbStaffName2").send_keys("肖新苗")
-browser.find_element_by_id("TabContainer1_tabDetail_tbPwd").send_keys("1234")
-
-browser.switch_to.parent_frame()
-
-time.sleep(5)
-
-browser.find_element_by_xpath('//*[@id="app-tabs-body"]//div/a/span/span/span[text()=" 保存 "]').click()
-
-time.sleep(5)
-browser.find_element_by_xpath('//*[@id="messagebox-1001"]//div/a[2]').click()
 
 
 
 #2.查询
-time.sleep(5)
+time.sleep(sleepTime)
 
 browser.switch_to.frame(browser.find_element_by_xpath("//iframe[contains(@src,'staff.aspx?opmode=m&menuid=110306')]"))
 
 browser.find_element_by_id("__tab_TabContainer1_tabEnquiry").click()
-browser.find_element_by_id("TabContainer1_tabEnquiry_tbStaffCode").send_keys(id)
+#browser.find_element_by_id("TabContainer1_tabEnquiry_tbStaffCode").send_keys(id)
+browser.find_element_by_id("TabContainer1_tabEnquiry_tbStaffName").send_keys("肖新苗")
 
 browser.switch_to.parent_frame()
-time.sleep(5)
+time.sleep(sleepTime)
 browser.find_element_by_xpath('//*[@id="app-tabs-body"]//div/a/span/span/span[text()=" 查询 "]').click()
 
 #3.删除
-
-time.sleep(5)
+time.sleep(sleepTime)
 browser.switch_to.frame(browser.find_element_by_xpath("//iframe[contains(@src,'staff.aspx?opmode=m&menuid=110306')]"))
+for index in range(0,2):
+    time.sleep(sleepTime)
+    print(browser.find_element_by_id("TabContainer1_tabEnquiry_gvStaff").find_element_by_xpath('//*[text()="'+str(int(id)+index)+'"]/../following-sibling::*'))
+    browser.find_element_by_id("TabContainer1_tabEnquiry_gvStaff").find_element_by_xpath('//*[text()="'+str(int(id)+index)+'"]/../../td[1]/input').click()
 
 
-time.sleep(5)
-
-browser.find_element_by_id("TabContainer1_tabEnquiry_gvStaff_ctl02_cbSelect").click()
-
-
-time.sleep(5)
-
+time.sleep(sleepTime)
 browser.switch_to.parent_frame()
-time.sleep(5)
+time.sleep(sleepTime)
 browser.find_element_by_xpath('//*[@id="app-tabs-body"]//div/a/span/span/span[text()=" 删除 "]').click()
 
-time.sleep(5)
+time.sleep(sleepTime)
 
 browser.find_element_by_xpath('//*[@id="messagebox-1001-toolbar"]//div/a[2]').click()
+browser.switch_to.parent_frame()
